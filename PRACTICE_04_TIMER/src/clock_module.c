90cf527d3f6fd22c71c23b2c6f6b1e37af0227fa
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2024-08-16 17:43:13 -0300

    Delete PRACTICE_04_TIMER directory

--- PRACTICE_04_TIMER/src/clock_module.c
+++
@@ -1,7 +0,0 @@
-#include "clock_module.h"
-#include "soc_AM335x.h"
-
-void ckmSetCLKModuleRegister(CKM_MODULE_REG module, unsigned int value){
-    unsigned int addr_temp = SOC_CONTROL_REGS + module;
-    addr_temp |= value;
-}
