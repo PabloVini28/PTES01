1fcf3993bbdb5399f76b71d5728639025c826cbe
Pablo Vinícios <128994831+PabloVini28@users.noreply.github.com>
2024-08-24 01:54:50 -0300

    Delete Practice04_Timer directory

--- Practice04_Timer/src/control_module.c
+++
@@ -1,7 +0,0 @@
-#include "control_module.h"
-#include "soc_AM335x.h"
-
-void cmSetCtrlModule(CONTROL_MODULE offset, unsigned int value){
-    unsigned int aux = SOC_CONTROL_REGS + offset;
-    aux |= value;
-}
